e9641f2198b6cbcd70791904bb6979eee6677d89
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2025-06-01 03:24:36 +0200

    0.0.1

--- control.lua
+++ control.lua
@@ -10,9 +10,10 @@ script.on_event(defines.events.on_lua_shortcut,
 
 script.on_event(defines.events.on_player_selected_area, function(event)
   local player = game.players[event.player_index]
+  local n = player.mod_settings["dead-tree-deconstructor-percentage"].value / 100
   if event.item == "dead-tree-deconstructor" then
     for _, entity in pairs(event.entities) do
-      if entity.tree_stage_index >= entity.tree_stage_index_max then
+      if entity.tree_stage_index_max == 0 or entity.tree_stage_index / entity.tree_stage_index_max >= n then
         entity.order_deconstruction(player.force, player)
       end
     end
---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "dead-tree-deconstructor-percentage",
+    setting_type = "runtime-per-user",
+    default_value = 100,
+    minimum_value = 0,
+    maximum_value = 100,
+  },
+})
